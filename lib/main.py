6fe37e7fd5749e61b29819d478b6075f46e4124d
Brando <brandoflores16@gmail.com>
2020-12-15 21:03:17 -0700

    Add turtle movement and directions and clear

--- lib/main.py
+++ lib/main.py
@@ -6,7 +6,26 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.reset()
+
 screen.listen()
 #Don't add parenthesis to function parameter, else it will trigger immediately
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
 screen.exitonclick()
